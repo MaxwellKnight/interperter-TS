fd21891291abf674b76885ae3f8d22981b157ec4
Maxwell Knight <maxwell.knight98@gmail.com>
2024-05-10 04:36:15 +0300

    Refactored main, extracted dot logic

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ import { Evaluator, envs } from './src/evaluator';
 import { Enviroment } from './src/enviroment';
 import { exec } from 'child_process';
 import fs from 'fs';
+import { dotToPdf, createEnvironmentDot } from './src/graphviz';
 
 const prompt = promptSync();
 class REPL {
@@ -70,55 +71,3 @@ class REPL {
 	// No arguments provided, run the interactive REPL
 	repl.run();
 })();
-
-/// Function to create a DOT representation from an array of environments
-function createEnvironmentDot(environments: Enviroment[]) {
-	let dot = "digraph Environment {\n"; 
- 
-	const envNodes = new Map(); 
-	let envCounter = 0;
- 
-	const getEnvNode = (env: Enviroment | null) => {
-	  if (!envNodes.has(env)) {
-		 const nodeName = `Env${envCounter++}`;
-		 envNodes.set(env, nodeName);
-		 return nodeName;
-	  }
-	  return envNodes.get(env);
-	};
- 
-	environments.forEach((env: Enviroment, index: number) => {
-		const currentEnvNode = getEnvNode(env);
-		const vars: string[] = [];
-		env.getEnv().forEach((obj, key) => vars.push(`${key}: ${obj.stringify()}`));
-		const variables = vars.join("\\n");
-
-		dot += `  ${currentEnvNode} [label="${variables}" shape="box"];\n`;
-
-		if (env.getParent()) {
-			const parentEnvNode = getEnvNode(env.getParent());
-			dot += `  ${currentEnvNode} -> ${parentEnvNode} [label=Env${index}];\n`;
-		}
-	});
- 
-	dot += "}"; // End of the DOT graph
-	return dot;
- }
-
- // Function to convert a DOT string into a PDF
-function dotToPdf(dot: string, outputFilePath: string) {
-	const tempDotFilePath = "temp_dot_file.dot";
-	fs.writeFileSync(tempDotFilePath, dot, "utf-8");
-
-	const command = `dot -Tpdf ${tempDotFilePath} -o ${outputFilePath}`;
-	exec(command, (error, stdout, stderr) => {
-	  if (error) {
-		 console.error("Error executing Graphviz command:", error);
-	  } else {
-		 console.log(`PDF successfully generated at ${outputFilePath}`);
-	  }
- 
-	  // Clean up the temporary DOT file
-	  fs.unlinkSync(tempDotFilePath);
-	});
-}
---
+++ src/graphviz.ts
@@ -0,0 +1,49 @@
+import { exec } from "child_process";
+import { Enviroment } from "./enviroment";
+import fs from 'fs';
+
+export const createEnvironmentDot = (environments: Enviroment[]) => {
+	let dot = "digraph Environment {\n"; 
+
+	const envNodes = new Map(); 
+	let envCounter = 0;
+
+	const getEnvNode = (env: Enviroment | null) => {
+		if (!envNodes.has(env)) {
+			const nodeName = `Env${envCounter++}`;
+			envNodes.set(env, nodeName);
+			return nodeName;
+		}
+		return envNodes.get(env);
+	};
+
+	environments.forEach((env: Enviroment, index: number) => {
+		const currentEnvNode = getEnvNode(env);
+		const vars: string[] = [];
+		env.getEnv().forEach((obj, key) => vars.push(`${key}: ${obj.stringify()}`));
+		const variables = vars.join("\\n");
+
+		dot += `  ${currentEnvNode} [label="${variables}" shape="box"];\n`;
+
+		if (env.getParent()) {
+			const parentEnvNode = getEnvNode(env.getParent());
+			dot += `  ${currentEnvNode} -> ${parentEnvNode} [label=Env${index}];\n`;
+		}
+	});
+
+	dot += "}"; 
+	return dot;
+}
+
+export const dotToPdf = (dot: string, outputFilePath: string) => {
+	const tempDotFilePath = "temp_dot_file.dot";
+	fs.writeFileSync(tempDotFilePath, dot, "utf-8");
+
+	const command = `dot -Tpdf ${tempDotFilePath} -o ${outputFilePath}`;
+	exec(command, (error, stdout, stderr) => {
+		if (error) 	console.error("Error executing Graphviz command:", error);
+		else 			console.log(`PDF successfully generated at ${outputFilePath}`);
+		
+		fs.unlinkSync(tempDotFilePath);
+	});
+}
