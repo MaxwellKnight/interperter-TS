5dceae1ae7e13a62ca9af88a8152218afc93e9af
Maxwell Knight <maxwell.knight98@gmail.com>
2024-05-15 06:08:59 +0300

    deleted build and added it to gitignore

--- build/jest.config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.testEnvironment = exports.preset = void 0;
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-exports.preset = 'ts-jest';
-exports.testEnvironment = 'node';
